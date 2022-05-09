7128341d9fbfef534f25b17b418caad619a669a8
Squirkey89 <squirke89@gmail.com>
2022-05-09 10:43:44 +0000

    a

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codestar-220022.herokuapp.com', 'localhost']
+
 
 
 # Application definition
